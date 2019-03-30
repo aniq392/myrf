49a3bbb05a1e3952213c33584b24439fb10d3730
aniq park <aniq@chol.com>
2019-03-30 22:42:35 +0900

    Signed-off-by: aniq park <aniq@chol.com>

---
+++ aniqbest/index.php
@@ -0,0 +1,357 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- header begin -->
+<!-- ref : https://app.bytescout.com/barcode-reader/barcode-reader/qr-code-reader.html -->
+<!-- ref: https://html-color-codes.info/Korean/ -->
+
+<style>
+  
+  @media print {
+  #footer .container
+  {
+    display:none;
+  }
+  
+  #header .container
+  {
+    display:none;
+  }
+}
+
+a{
+   color: #7c943b; 
+ }
+ 
+</style>
+
+<!-- header end -->
+
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  
+  <title>AniQ Best of Web Blog</title>
+  <meta content="free tools online for lists sorting, regular expressions, base 64 encoding and decoding, word counting and much more" name="description" />
+  <meta content="free tools online, free applications online, tools, free apps, bytescout, online" name="keywords" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css" rel="stylesheet"  crossorigin="anonymous"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <style>
+    a{
+      color: #7c943b;
+    }
+     a:hover{
+       text-decoration: none;    
+     }
+   
+   
+   .dashboard-box {
+     width: auto;
+     padding: 10px;
+     border: 1px solid lightgray;
+     height: 148px;
+     margin-bottom: 14px;
+     border-radius: 11px;
+     text-align: center;
+     font-weight: 400;
+     box-shadow: 4px 4px;
+     background-color: #eff0f1;
+     }
+     .dashboard-box i {
+       font-size: 18px; 
+       }
+     
+     .dashboard-box i.merged {
+       margin: 0 0 0 -3px;
+       font-size: 42px; }
+     .dashboard-box .box-header {
+       display: flex;
+       justify-content: space-between;
+       align-items: center; 
+       text-decoration:none;    
+       }
+       .dashboard-box .box-header .dashboard-title {
+         margin: 0; }
+     .dashboard-box .box-body .content {
+       font-size: 14px; }
+   
+   
+   .dashboard-box:hover{
+     background-color: white;
+     color: #7c943b;
+   }
+   
+   .dashboard-box > div:first-child {
+     overflow: hidden; }
+   
+   .dashboard-title {
+     padding: 3px 3px 3px; 
+     }
+   
+   @media screen and (min-width: 320px) and (max-width: 567px) {
+     .dashboard-box .dashboard-title {
+       font-size: 13px;
+       padding: 0px 10px 0px; }
+     .dashboard-box img.pull-right, .dashboard-box i.pull-right {
+       height: 35px;
+       font-size: 40px;
+       width: auto; } }
+   @media screen and (min-width: 375px) and (max-width: 667px) {
+     .dashboard-title {
+       font-size: 15px;
+       padding: 3px 10px 0px;
+       margin-left: 0px; } }
+   @media screen and (min-width: 992px) and (max-width: 1200px) {
+     .dashboard-title {
+       padding: 0px 10px 0px; }
+   
+     .dashboard-box i.pull-right {
+       font-size: 53px; } }
+   
+     </style>
+       
+   </head>
+   
+   <body>
+
+<!-- header body begin -->
+
+    <header class="navbar navbar-default navbar-static-top" id="navbar">
+        <div class="container">
+            <div class="row col-md-12">
+
+                <div class="col-md-12">
+
+                    <div class="text-muted">
+
+                            <a href="./index.php">
+                                <span style="font-family: Impact;">
+                                    <font color="red">AniQ</font>
+                                    <font color="#7c943b">Best</font>
+                                </span>
+                            </a>                        
+
+                        &nbsp;&nbsp;&nbsp;
+
+                        <small>                            
+                            <a href="/index.html">도메인 하나</a> - 
+                            <a href='/data-playground/index.html'><strong>도메인 둘</strong></a> - 
+                            <a href='/qr-code-reader/qr-code-reader.html'>도메인 셋</a>                        
+                        </small>                     
+                                                
+
+                        <div id="share" class="text-center">      
+                            <small>
+                                소셜 미디어:
+                                  <a 
+                                  href="./index.php" target="_blank" title="Share on Facebook" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&amp;t=' + encodeURIComponent(document.URL)); return false;">
+                                      Facebook
+                                  </a>                            
+                            &nbsp;-&nbsp;                             
+                                  <a 
+                                  href="./index.php" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20' + encodeURIComponent(document.URL)); return false;">
+                                    Twitter
+                                  </a>
+                            &nbsp;-&nbsp;                                   
+                                  <a 
+                                  href="./index.php" target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&amp;description=' +  encodeURIComponent(document.title)); return false;">
+                                    Pinterest
+                                  </a>
+                            &nbsp;-&nbsp; 
+                                  <a 
+                                  href="./index.php" target="_blank" title="Submit to Reddit" onclick="window.open('http://www.reddit.com/submit?url=' + encodeURIComponent(document.URL) + '&amp;title=' +  encodeURIComponent(document.title)); return false;">
+                                    Reddit
+                                  </a>
+                            &nbsp;-&nbsp; 
+                                  <a 
+                                  href="mailto:" target="_blank" title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&amp;body=' +  encodeURIComponent(document.URL)); return false;">
+                                    Email
+                                  </a>
+                            </small>
+                        </div>                                              
+                    </div>
+                </div>
+
+                <!--
+                <div class="col-lg-1">
+                    <div class="alert">
+                        &nbsp;
+                    </div>
+                </div>
+                -->
+            </div>
+        </div>
+    </header>
+
+<!-- header body end -->        
+   
+     <div role='main' class='container'>
+   
+       <div class='row'>
+           <div class='col-md-12 text-center'>
+               <h1>AniQBest of ILoveBee.kr</h1>
+           </div>
+       </div>
+   
+       <div class="container">
+           <div class="row dashboard-content">   
+
+              <div class="col-md-3">
+                  <a href="http://ilovebee.kr/"> 
+                    <div class="dashboard-box">
+                      <div class="box-header">
+                          <div class="dashboard-title">
+                            <i class="fa fa-qrcode fa-2x pull-left"></i>                  
+                            CSV Data Playground
+                          </div>              
+                      </div>
+                      <div class="box-body">
+                          <p class="content">
+                            I Love Bee of KR
+                          </p>
+                      </div>            
+                    </div>
+                  </a>
+              </div>                           
+
+
+              <div class="col-md-3">
+                  <a href="barcode-reader/qr-code-reader.html"> 
+                    <div class="dashboard-box">
+                      <div class="box-header">
+                          <div class="dashboard-title">
+                            <i class="fa fa-qrcode fa-2x pull-left"></i>                  
+                            QR Code Decoder Online
+                          </div>              
+                      </div>
+                      <div class="box-body">
+                          <p class="content">
+                            read QR Code from live camera or from a file or image, online and free.
+                          </p>
+                      </div>            
+                    </div>
+                  </a>
+              </div>                
+              
+
+              <div class="col-md-3">
+                  <a href="barcode-reader/barcode-reader.html"> 
+                    <div class="dashboard-box">
+                      <div class="box-header">
+                          <div class="dashboard-title">
+                            <i class="fa fa-qrcode fa-2x pull-left"></i>                  
+                            BarCode Reader Online
+                          </div>              
+                      </div>
+                      <div class="box-body">
+                          <p class="content">
+                            read QR Code, Code 128, Code 39 and other barcodes from live camera or from file
+                          </p>
+                      </div>            
+                    </div>
+                  </a>
+              </div>                
+
+   
+               <div class="col-md-3">
+                   <a href="qrcode-barcode-generator/qrcode-barcode-generator.html"> 
+                     <div class="dashboard-box">
+                       <div class="box-header">
+                           <div class="dashboard-title">
+                             <i class="fa fa-qrcode fa-2x pull-left"></i>                  
+                             QR Code Generator
+                           </div>              
+                       </div>
+                       <div class="box-body">
+                           <p class="content">
+                             End 
+                           </p>
+                       </div>            
+                     </div>
+                   </a>
+               </div>          
+
+           </div>
+       </div>
+   
+     </div>      
+     
+   </body>
+<!-- footer begin -->
+<div class='container small'>
+    <class id="footer" class='footer'>
+
+
+    <div class='row'>&nbsp;</div>
+    <div class='row'>&nbsp;</div>
+
+    <div class='container'>
+        <div class='col-lg-12 col-md-12 small text-info'>
+        <!--SEO-FOOTER-->
+        </div>
+    </div>
+
+
+    <div class='container'>                    
+        <div class="row">
+            <hr/>
+        </div>
+        
+
+        <div class='row'>
+                <br/>    
+                <div style="text-align: left;" class="addthis_toolbox addthis_default_style addthis_32x32_style">
+                        <a class="addthis_button_preferred_1"></a>
+                        <a class="addthis_button_preferred_2"></a>  
+                        <a class="addthis_button_preferred_3"></a>
+                        <a class="addthis_button_preferred_4"></a>
+                        <a class="addthis_button_compact"></a>
+                        </div>
+                                            
+                        <br/>
+                        <br/>                
+                    
+            </div>        
+
+        <div class="row">
+            <div class="col-lg-12">
+
+                <ul class="list-unstyled">
+                    <li class="float-lg-right">
+                        <a href="#top">Back to top</a>
+                    </li>
+
+                    <li>
+                        <a href='./aniqbest/privacy.html'> 개인정보 보호 정책(준비중) </a>
+                    </li>
+                    <li>
+                        <a href='./aniqbest/terms.html'> 이용 약관 (준비중) </a>
+                    </li>
+
+                </ul>
+
+                <p id="copyright" class='text-muted text-small'>
+                    &copy; 2007 - 
+                    <script>
+                        document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+                    </script>
+                    <span>
+                        <a href='./aniqbest/tools.html'>AniQBest - tools for developers</a> 
+                    </span>
+                </p>
+
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+<!-- footer end -->
+</html>
+     
